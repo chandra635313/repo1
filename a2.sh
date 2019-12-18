3efc90fb3a9b4b20e3e3ad05b6a8ce00f7fc727f
chandra635313 <48780630+chandra635313@users.noreply.github.com>
2019-12-18 12:46:47 +0530

    Update a2.sh

--- a2.sh
+++ a2.sh
@@ -3,3 +3,6 @@ cd repo1
 git add .
 git commit -m " first commit "
 git push
+cp -r *  /var/www/html/   total folder lo unaa all files folders * means
+
+cp -r foldername/* /var/www/html/   total folder deploy 
